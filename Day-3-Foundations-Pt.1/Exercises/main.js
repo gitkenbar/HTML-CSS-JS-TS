36121eddf1bf8cde0656ea74a3cfcdcee880b4c4
gitkenbar <kennethbarker12@gmail.com>
2023-10-17 14:04:56 -0500

    Hopefully this works

---
+++ Day-3-Foundations-Pt.1/Exercises/main.js
@@ -0,0 +1,10 @@
+// Ex 1 - Basic JS
+console.log("Hello World");
+// Ex 2 - DataTypes and Variables
+var myName = "Kenneth";
+console.log(myName);
+// Ex 3 - Mathematical Computations
+function calcArea(L, H) {
+    return L * H;
+}
+calcArea(3, 4);
---
+++ Day-3-Foundations-Pt.1/Exercises/main.ts
@@ -0,0 +1,14 @@
+// Ex 1 - Basic JS
+console.log("Hello World")
+
+// Ex 2 - DataTypes and Variables
+let myName:string = "Kenneth";
+console.log(myName);
+
+// Ex 3 - Mathematical Computations
+function calcArea<T>()
+function calcArea(L:number, H:number){
+    return L * H}
+
+calcArea(3, 4)
+
